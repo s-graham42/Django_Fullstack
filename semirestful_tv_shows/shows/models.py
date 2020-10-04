b4a4f7024f58eedb3708d1650bcde86325dd3a65
Stephen Graham <sgraham42@gmail.com>
2020-10-04 00:48:45 -0700

    SR TV Shows Validated Complete

--- semirestful_tv_shows/shows/models.py
+++ semirestful_tv_shows/shows/models.py
@@ -1,6 +1,41 @@
+from __future__ import unicode_literals
 from django.db import models
+from datetime import *
 
 # Create your models here.
+class ShowManager(models.Manager):
+    def new_validator(self, postData):
+        errors = {}
+        if len(postData['title']) < 2:
+            errors['title'] = "Title must be at least 2 characters."
+        if Show.objects.filter(title=postData['title']):
+            errors['duplicate_title'] = "Title must be unique."
+        if len(postData['network']) < 3:
+            errors['network'] = "Network must be at least 3 characters."
+        if len(postData['release_date']) == 0:
+            errors['no_release_date'] = "Please enter a Release Date"
+        elif datetime.strptime(postData['release_date'], '%Y-%m-%d') > datetime.today():
+            errors['release_date'] = "Release Date must be in the past."
+        if len(postData['description']) < 10 and len(postData['description']) > 0:
+            errors['description'] = "If a Description is entered, it must be at least 10 characters."
+        return errors
+
+    def edit_validator(self, postData, show_id):
+        errors = {}
+        if len(postData['title']) < 2:
+            errors['title'] = "Title must be at least 2 characters."
+        if Show.objects.filter(title=postData['title']).exclude(id=show_id):
+            errors['duplicate_title'] = "Title must be unique."
+        if len(postData['network']) < 3:
+            errors['network'] = "Network must be at least 3 characters."
+        if len(postData['release_date']) == 0:
+            errors['no_release_date'] = "Please enter a Release Date"
+        elif datetime.strptime(postData['release_date'], '%Y-%m-%d') > datetime.today():
+            errors['release_date'] = "Release Date must be in the past."
+        if len(postData['description']) < 10 and len(postData['description']) > 0:
+            errors['description'] = "If a Description is entered, it must be at least 10 characters."
+        return errors
+
 class Show(models.Model):
     title = models.CharField(max_length = 255)
     network = models.CharField(max_length = 64)
@@ -8,3 +43,4 @@ class Show(models.Model):
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = ShowManager()
--- semirestful_tv_shows/shows/views.py
+++ semirestful_tv_shows/shows/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from .models import *
-
+from .models import Show
+from django.contrib import messages
+from datetime import date
 # Create your views here.
 def index(request):
     return redirect('/shows')
@@ -16,10 +17,15 @@ def new(request):
 
 def create(request):
     if request.method == "POST":
-        print(request.POST)
-        print(request.POST['release_date'])
-        new_show = Show.objects.create(title=request.POST['title'], network=request.POST['network'], release_date=request.POST['release_date'], description=request.POST['description'])
-    return redirect('/shows/' + str(new_show.id))
+        errors = Show.objects.new_validator(request.POST)
+        if len(errors) > 0:
+            for key, value in errors.items():
+                messages.error(request, value)
+            return redirect('/shows/new')
+        else:
+            new_show = Show.objects.create(title=request.POST['title'], network=request.POST['network'], release_date=request.POST['release_date'], description=request.POST['description'])
+            return redirect('/shows/' + str(new_show.id))
+    return redirect('/shows/new')
 
 def one_show(request, show_id):
     context = {
@@ -35,13 +41,20 @@ def edit(request, show_id):
 
 def update(request, show_id):
     if request.method == "POST":
-        show_to_update = Show.objects.get(id=show_id)
-        show_to_update.title = request.POST['title']
-        show_to_update.network = request.POST['network']
-        show_to_update.release_date = request.POST['release_date']
-        show_to_update.description = request.POST['description']
-        show_to_update.save()
-    return redirect('/shows/' + str(show_id))
+        errors = Show.objects.edit_validator(request.POST, show_id)
+        if len(errors) > 0:
+            for key, value in errors.items():
+                messages.error(request, value)
+            return redirect('/shows/'+ str(show_id) + '/edit')
+        else:
+            show_to_update = Show.objects.get(id=show_id)
+            show_to_update.title = request.POST['title']
+            show_to_update.network = request.POST['network']
+            show_to_update.release_date = request.POST['release_date']
+            show_to_update.description = request.POST['description']
+            show_to_update.save()
+            return redirect('/shows/' + str(show_id))
+    return redirect('/')
 
 def destroy(request, show_id):
     if request.method == "POST":
