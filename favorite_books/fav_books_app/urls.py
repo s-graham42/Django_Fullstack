7bd737e0f7dfdb644d21251f20a7626830969665
Stephen Graham <sgraham42@gmail.com>
2020-10-12 14:38:18 -0700

    Favorite Books Complete (no Sensei)

--- favorite_books/fav_books_app/urls.py
+++ favorite_books/fav_books_app/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('create', views.create),
     path('<int:book_id>', views.show),
     path('<int:book_id>/like', views.like),
+    path('<int:book_id>/un_like', views.un_like),
+    path('<int:book_id>/update', views.update),
+    path('<int:book_id>/destroy', views.destroy),
 ]
--- favorite_books/fav_books_app/views.py
+++ favorite_books/fav_books_app/views.py
@@ -66,3 +66,43 @@ def like(request, book_id):
     curr_user = User.objects.get(id=request.session['current_user'])
     this_book.liked_by.add(curr_user)
     return redirect (f'/books/{book_id}')
+
+def un_like(request, book_id):
+    if 'current_user' not in request.session:
+        messages.error(request, "Please register or log in.")
+        return redirect('/')
+    this_book = Book.objects.filter(id=book_id)
+    if len(this_book) == 0:
+        messages.error(request, "Book id doesn't exist.")
+        return redirect('/books')
+    this_book = this_book[0]
+    curr_user = User.objects.get(id=request.session['current_user'])
+    this_book.liked_by.remove(curr_user)
+    return redirect (f'/books/{book_id}')
+
+def update(request, book_id):
+    if 'current_user' not in request.session:
+        messages.error(request, "Please register or log in.")
+        return redirect('/')
+    if request.method == "POST":
+        errors = Book.objects.book_validator(request.POST)
+        if len(errors) > 0:
+            for key, value in errors.items():
+                messages.error(request, value)
+            return redirect(f'/books/{book_id}')
+        this_book = Book.objects.get(id=book_id)
+        this_book.title = request.POST['title']
+        this_book.desc = request.POST['description']
+        this_book.save()
+        return redirect(f'/books/{book_id}')
+    return redirect(f'/books')
+
+def destroy(request, book_id):
+    if 'current_user' not in request.session:
+        messages.error(request, "Please register or log in.")
+        return redirect('/')
+    if request.method == "POST":
+        this_book = Book.objects.get(id=book_id)
+        this_book.delete()
+        return redirect(f'/books')
+    return redirect(f'/books/{book_id}')
